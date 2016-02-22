82c0efc6895a3cb76d8454f733b9af615432aaa5
Damian Maguire <damian@sciantechnologies.com>
2016-02-22 21:12:04 +0000

    [Queue] Initial implmentation of the Queue interface
    Adding the initial interface of the Queue class.

--- MamaPy.pyx
+++ MamaPy.pyx
@@ -84,4 +84,88 @@ cdef class Mama:
     def getLastStatus (self):
         return self.status
 
+    """ Getters """
+    cdef mamaBridge getBridge (self):
+        return self.bridge
 
+
+cdef class Queue:
+    cdef mamaQueue queue
+    cdef mamaBridge bridge
+    cdef mama_status status
+
+    def __cinit__ (self, bridge):
+        bridge = bridge.getBridge()
+        status = mamaQueue_create (&self.queue, self.bridge)
+
+    # def __dealloc__ (self):
+    #     status = mamaQueue_destroy (self.queue)
+
+    def destroy (self):
+        status = mamaQueue_destroy (self.queue)
+        return status
+
+    def destroyWait (self):
+        status = mamaQueue_destroyWait (self.queue)
+        return status
+
+    def destroyTimedWait (self, timeout):
+        status = mamaQueue_destroyTimedWait (self.queue, timeout)
+        return status
+
+    def canDestroy (self):
+        status = mamaQueue_canDestroy (self.queue)
+        return status
+
+    def setHighWatermark (self, highWatermark):
+        status = mamaQueue_setHighWatermark (self.queue, highWatermark)
+        return status
+
+    def getHighWatermark (self):
+        cdef size_t highWatermark
+        status = mamaQueue_getHighWatermark (self.queue, &highWatermark)
+        return highWatermark
+
+    def setLowWatermark (self, lowWatermark):
+        status = mamaQueue_setLowWatermark (self.queue, lowWatermark)
+        return status
+
+    def getLowWatermark (self):
+        cdef size_t lowWatermark
+        status = mamaQueue_getLowWatermark (self.queue, &lowWatermark)
+        return lowWatermark
+
+    def getEventCount (self):
+        cdef size_t count
+        status = mamaQueue_getEventCount (self.queue, &count)
+        return count
+
+    def setQueueName (self, name):
+        status = mamaQueue_setQueueName (self.queue, name)
+        return status
+
+    def getQueueName (self):
+        cdef const char *name
+        status = mamaQueue_getQueueName (self.queue, &name)
+        return name
+
+    def getQueueBridgeName (self):
+        cdef const char *name
+        status = mamaQueue_getQueueBridgeName (self.queue, &name)
+        return name
+
+    def dispatch (self):
+        status = mamaQueue_dispatch (self.queue)
+        return status
+
+    def timedDispatch (self, timeout):
+        status = mamaQueue_timedDispatch (self.queue, timeout)
+        return status
+
+    def dispatchEvent (self):
+        status = mamaQueue_dispatchEvent (self.queue)
+        return status
+
+    def stopDispatch (self):
+        status = mamaQueue_stopDispatch (self.queue)
+        return status
